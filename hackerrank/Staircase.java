26455263fe3da4773a6e540f2839ac4766328801
aaali-software <aziz.ali.software@gmail.com>
2026-05-04 20:02:53 -0400

    add staircase problem

---
+++ hackerrank/Staircase.java
@@ -0,0 +1,16 @@
+package hackerrank;
+
+public class Staircase {
+
+    public static void staircase(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.println(" ".repeat(n - i) + "#".repeat(i));
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        staircase(n);
+    }
+
+}
